6a13a54a50f5b4a19b65df874c6d7f840f76fd2b
Afonso Costa <arabelo@gmail.com>
2023-05-23 11:10:42 +0200

    Show and remove trailing spaces on save

--- init.lua
+++ init.lua
@@ -5,3 +5,13 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+local cmd = vim.cmd
+
+-- Highlight whitespaces
+cmd([[highlight WhitespaceEOL ctermbg=red ctermfg=white guibg=red guifg=white]])
+cmd([[match WhitespaceEOL /\s\+$/]])
+
+-- Trim whitespace onsave
+cmd([[autocmd FileType c,cpp,java,php,html,javascript,css,python,sh,lua autocmd BufWritePre <buffer> %s/\s\+$//e]])
+

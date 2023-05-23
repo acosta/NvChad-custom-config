57256215b365d3f9068c24b963a2651ea4f2fc72
Afonso Costa <arabelo@gmail.com>
2023-05-23 11:52:17 +0200

    Enable autoformatting on save

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -4,30 +4,58 @@ if not present then
   return
 end
 
-local b = null_ls.builtins
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
 
   -- Lua
-  b.formatting.stylua,
+  formatting.stylua,
 
   -- cpp
-  b.formatting.clang_format,
+  formatting.clang_format,
 
   -- python
-  b.formatting.black,
-  b.formatting.isort,
-  b.diagnostics.ruff,
+  formatting.black,
+  formatting.isort,
+  diagnostics.ruff,
 
   -- markdown
-  b.diagnostics.markdownlint
+  diagnostics.markdownlint,
 }
+
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format {
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  }
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
+end
 
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = on_attach,
 }
